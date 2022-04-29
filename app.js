8998d0190a833ceee8e34ea3ea773f236316863d
Drake Alia <52472741+DrakeAlia@users.noreply.github.com>
2022-04-29 12:11:13 -0700

    setup my nav bar functionality with index.html and app.js files

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
-// classList - shows/gets all classes
-// contains - checks classList for specific class
-// add - add class
-// remove - remove class
-// toggle - toggles class
+// classList - shows/gets all classes (X)
+// contains - checks classList for specific class (X)
+// add - add class (x)
+// remove - remove class (x)
+// toggle - toggles class (x)
+
+const navToggle = document.querySelector(".nav-toggle")
+const links = document.querySelector(".links")
+
+navToggle.addEventListener("click", function () {
+    // console.log(links.classList)
+    // console.log(links.classList.contains("random"))
+    // console.log(links.classList.contains("links"))
+    // if (links.classList.contains("show-links")) {
+    //     links.classList.remove("show-links")
+    // }
+    // else {
+    //     links.classList.add("show-links")
+    // }
+    // OR:
+    links.classList.toggle("show-links")
+})
